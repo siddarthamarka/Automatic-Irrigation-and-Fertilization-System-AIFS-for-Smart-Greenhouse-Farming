6ba8c4dfde2328ff0b89241571fcc97aa1b57610
SIDDARTHA MARKA <159034788+siddarthamarka@users.noreply.github.com>
2025-07-14 13:36:03 +0530

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const sendControlCommand = (command) => {
-  return axios.post('/api/control', { command });
-};
-
-export const fetchStatus = () => {
-  return axios.get('/api/status');
-};
